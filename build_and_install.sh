762f9d4baf75487916b1d214a0b3e85264db61c8
Leone, Mark A [LGS] <mleone607@gmail.com>
2026-01-11 18:40:49 -0500

    Use python vs pyhton3

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,4 +1,4 @@
 ./install_deps.sh
 python -m pip install -r requirements.txt
-python3 -m PyInstaller --onedir --name led_mon --add-data plugins:plugins --add-data snapshot_files:snapshot_files --add-data config.yaml:. --hidden-import=yaml --hidden-import=pynput --clean --noconfirm main.py
+python -m PyInstaller --onedir --name led_mon --add-data plugins:plugins --add-data snapshot_files:snapshot_files --add-data config.yaml:. --hidden-import=yaml --hidden-import=pynput --clean --noconfirm main.py
 ./install_service.sh
